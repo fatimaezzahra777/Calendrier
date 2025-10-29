ee444b71b257b2aef8dbc400ad8341506a42990b
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2025-10-29 12:39:54 +0100

    supprimer partie dans html

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const form = document.querySelectorAll("td");
 const formul = document.querySelector('.formul');
 const semaine = document.querySelector(".semaine");
-const semaines = document.querySelector(".semaine");
 
 form.forEach(element =>{
   element.addEventListener('click', () => {
@@ -16,8 +15,6 @@ btn.addEventListener('click', () => {
    btn.style.display ='none';
 })
 
-const valeurs = Array.map(td => td.textContent);
-console.log(valeurs); 
 
 
 
