1e82d99594d0440d307488dabc43d4605cd19cd0
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2025-10-29 19:52:01 +0100

    ajoute modification

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const form = document.querySelectorAll("td");
 const formul = document.querySelector('.formul');
 const semaine = document.querySelector(".semaine");
+const formulaire = document.querySelector(".form-calendrier");
+
+
 
 form.forEach(element =>{
   element.addEventListener('click', () => {
-    formul.style.display = 'block'; 
+    formul.style.display = 'block';
+    formulaire.style.display = "flex"
   });
 });
 
 const btn = document.querySelector("#button");
 
-
 btn.addEventListener('click', () => {
    btn.style.display ='none';
 })
 
+for (let i = 0; i <= form.length; i++) {
+    form[i].textContent = i+1;
+}
 
 
 
