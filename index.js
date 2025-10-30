f4b2d712ac46e983379868c2e9494d26326b713e
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2025-10-30 10:20:43 +0100

    ajoute modification

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-const form = document.querySelectorAll(".active-day");
+const jour = document.querySelectorAll(".active-day");
 const formul = document.querySelector('.formul');
 const semaine = document.querySelector(".semaine");
 const formulaire = document.querySelector(".form-calendrier");
+const casee = document.querySelectorAll("td");
+const btn = document.querySelector("#button");
 
 
+for (let i = 0; i < casee.length; i++) {
+    casee[i].textContent = i+1;
+} 
 
-form.forEach(element =>{
+let casse = null;
+
+casee.forEach(element =>{
   element.addEventListener('click', () => {
     formul.style.display = 'block';
-    formulaire.style.display = "flex"
+    formulaire.style.display = "flex";
   });
 });
 
-const btn = document.querySelector("#button");
 
-btn.addEventListener('click', () => {
-   btn.style.display ='none';
+
+const colors = {
+  VIP: "bg-danger text-white",
+}
+
+
+btn.addEventListener('click', (event) => {
+   event.preventDefault();
+
+   const nom = document.getElementsById("nom").value;
+   const HeurDebut = document.querySelector("#heure").value;
+   const HeurFin = document.querySelector("#date").value;
+   const date = document.querySelector("#time").value;
+   const num = document.querySelector("#nombre").value;
+   const type = document.querySelector("#reserv").value;
+
+   if (!nom || !HeurDebut || !HeurFin || !date || !num || !type) {
+    alert("Veuillez remplir tous les champs.");
+    return;
+  }
+
+
 })
 
-for (let i = 0; i <= form.length; i++) {
-    formul[i].textContent = i+1;
-}
+
 
 
 
